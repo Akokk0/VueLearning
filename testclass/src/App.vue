8d04d588d4de34625dd990f06ecaa8e9cd30f141
akokko <zhminecraft@sina.com>
2022-08-15 22:52:54 +0800

    Test class

---
+++ testclass/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="test">Test</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  name: 'App',
+  methods: {
+    test() {
+      console.log(Vue)
+      console.log(Vue.prototype)
+      console.log(Vue.__proto__)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
